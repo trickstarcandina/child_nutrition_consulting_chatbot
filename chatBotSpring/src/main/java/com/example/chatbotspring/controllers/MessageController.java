62921549eb6da71f35e9cfd9fdbc3886cadbfd38
TruongNH <truong02bp@github.com>
2022-01-14 21:47:04 +0700

    fix cors

--- chatBotSpring/src/main/java/com/example/chatbotspring/controllers/MessageController.java
+++ chatBotSpring/src/main/java/com/example/chatbotspring/controllers/MessageController.java
@@ -3,6 +3,7 @@ package com.example.chatbotspring.controllers;
 import com.example.chatbotspring.services.MessageService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 @AllArgsConstructor
+@CrossOrigin("*")
 public class MessageController {
 
     private final MessageService messageService;
