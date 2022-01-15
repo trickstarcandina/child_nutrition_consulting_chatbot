2cbff76b2a033d9ddcef2bc3fbee77d94dee960d
TruongNH <truong02bp@github.com>
2022-01-15 09:46:37 +0700

    update

--- chatBotSpring/src/main/java/com/example/chatbotspring/data/entities/NhuCauNuoc.java
+++ chatBotSpring/src/main/java/com/example/chatbotspring/data/entities/NhuCauNuoc.java
@@ -3,7 +3,6 @@ package com.example.chatbotspring.data.entities;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -17,7 +16,7 @@ public class NhuCauNuoc extends BaseEntity {
     // tính theo kg
     private double canNang;
     // nước/kg/24h (ml) tối thiểu
-    private double khoiLuongToiThieu;
+    private double dungLuongToiThieu;
     // nước/kg/24h (ml) tối đa
-    private double khoiLuongToiDa;
+    private double dungLuongToiDa;
 }
